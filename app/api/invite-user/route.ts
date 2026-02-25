44b2ac892c5a5a9652c6196dd16efda5b5d115b4
prasannaramesh-make <prasanna.ramesh@techmobius.com>
2026-02-26 00:49:13 +0530

    Update route.ts

--- app/api/invite-user/route.ts
+++ app/api/invite-user/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server"
-import { supabaseAdmin } from "../../../../lib/supabase-admin"
+import { supabaseAdmin } from "../../../lib/supabase-admin"
 
 const ADMIN_EMAIL = "prasanna.ramesh@techmobius.com"
 
